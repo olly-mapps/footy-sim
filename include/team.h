e2a5f6626c118f7478db717eba484a921135d92f
Olly <98355735+olly-mapps@users.noreply.github.com>
2024-02-10 15:09:53 +0000

    added include

---
+++ include/team.h
@@ -0,0 +1,21 @@
+#ifndef __TEAM_H__
+#define __TEAM_H__
+
+class Team
+{
+
+    private:
+
+        // Skill Level
+        int skillStat;
+
+    public:
+
+    //Constructor
+    Team(int skill);
+
+    //Getters
+    int getSkillStat() const;
+};
+
+#endif 
